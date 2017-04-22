d6e10d85f807e9c725a6e0022aa7e5c5f17d03d0
AvalonQAQ <doublelegendary@foxmail.com>
2017-04-22 16:20:39 +0800

    vedio and setting

--- app/src/main/java/com/voice/User.java
+++ app/src/main/java/com/voice/User.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class User implements Serializable{
 
+
     private static final long serialVersionUID = 1L;
     private String LogId;
     private String LogName;
