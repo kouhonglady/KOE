3b9ee21d0e787dcb8225218e0268908b766cf190
AvalonQAQ <doublelegendary@foxmail.com>
2017-04-22 16:47:01 +0800

    vedio and setting

--- app/src/main/java/com/voice/User.java
+++ app/src/main/java/com/voice/User.java
@@ -3,7 +3,6 @@ package com.voice;
 import java.io.Serializable;
 
 public class User implements Serializable{
-
     private static final long serialVersionUID = 1L;
     private String LogId;
     private String LogName;
