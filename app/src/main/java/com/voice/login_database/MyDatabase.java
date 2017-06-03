127863a3aa4e4faf81785b6e49baa545a49bf04a
ExcaliburQAQ <1150790988@qq.com>
2017-06-03 16:12:06 +0800

    .

--- app/src/main/java/com/voice/login_database/MyDatabase.java
+++ app/src/main/java/com/voice/login_database/MyDatabase.java
@@ -36,7 +36,6 @@ public class MyDatabase extends SQLiteOpenHelper{
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
