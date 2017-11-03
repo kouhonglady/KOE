61f2425746dfbed424576450d4ae4bab98478a0c
lightningbaby <lightningbaby671@gmail.com>
2017-11-03 22:12:10 +0800

    语音识别功能修复啊哈哈哈哈Yeah！坐了一天，吃了一顿饭的成果！

--- app/src/main/java/com/voice/activity/TestActivity.java
+++ app/src/main/java/com/voice/activity/TestActivity.java
@@ -1,11 +1,18 @@
 package com.voice.activity;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import com.baidu.speech.EventListener;
+import com.baidu.speech.EventManager;
+import com.baidu.speech.EventManagerFactory;
+import com.baidu.speech.asr.SpeechConstant;
 import com.voice.MainActivity;
 import com.voice.R;
 import com.voice.wifi.foregin.Globals;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -13,6 +20,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -23,8 +35,11 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class TestActivity extends Activity implements OnClickListener {
+import org.json.JSONObject;
+
+public class TestActivity extends Activity implements EventListener, OnClickListener {
     private Context mContext;
     final MainActivity m=new MainActivity();
     private ImageButton last;//beforeone
@@ -34,6 +49,9 @@ public class TestActivity extends Activity implements OnClickListener {
     private TextView diff;
     private TextView pinyin;
     private TextView hanzi;
+    private TextView std_ans_text_view;
+    private TextView user_ans_text_view;
+    private ImageButton stop_test_image_button;
     private int numoflist = 7;
     private Activity mActivity;
     int testnum = m.hornor.gettestnum();
@@ -45,22 +63,37 @@ public class TestActivity extends Activity implements OnClickListener {
     private String result;
     private TextView score;
     int num_socre = 0;
+    private EventManager asr;
+
+    private String user_ans="";
+    private boolean logTime = true;
 
     public TestActivity() {
     }
 
-    ;
+    public String getUser_ans() {
+        return user_ans;
+    }
+
+    public void setUser_ans(String user_ans) {
+        this.user_ans = user_ans;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
+        setContentView(R.layout.test_two);
+        asr = EventManagerFactory.create(this, "asr");
+        asr.registerListener(this); //  EventListener 中 onEvent方法
 
         m.hornor.settestnum(testnum + 1);
         mActivity = TestActivity.this;
         currentnum = 0;
         initWidgets();
+        initPermission();
+
+
         UpdateView();
         // Set the application-wide context global, if not already set
         Context myContext = Globals.getContext();
@@ -90,8 +123,11 @@ public class TestActivity extends Activity implements OnClickListener {
         this.diff = (TextView) this.findViewById(R.id.diff);
         this.score = (TextView) this.findViewById(R.id.point);
         this.pinyin = (TextView) this.findViewById(R.id.pinyin);
+        this.std_ans_text_view= (TextView) this.findViewById(R.id.std_ans_text_view);
+        this.user_ans_text_view= (TextView) this.findViewById(R.id.user_ans_text_view);
+        this.stop_test_image_button= (ImageButton) this.findViewById(R.id.stop_test_image_button);
         this.hanzi = (TextView) this.findViewById(R.id.hanzi);
-        //	this.spelling=(TextView) this.findViewById(R.id.spelling);
+
 
 
         fayinBotton = (ImageButton) findViewById(R.id.fayin);
@@ -101,23 +137,18 @@ public class TestActivity extends Activity implements OnClickListener {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("com.baidu.action.RECOGNIZE_SPEECH");
-                intent.putExtra("sample", 16000); // 离线仅支持16000采样率
-                intent.putExtra("language", "cmn-Hans-CN"); // 离线仅支持中文普通话
-                intent.putExtra("prop", 20000); // 输入
-
-                //intent.putExtra(EXTRA_SOUND_START, R.raw.bdspeech_recognition_start);
-                //intent.putExtra(EXTRA_SOUND_END, R.raw.bdspeech_speech_end);
-                //intent.putExtra(EXTRA_SOUND_SUCCESS, R.raw.bdspeech_recognition_success);
-                //intent.putExtra(EXTRA_SOUND_ERROR, R.raw.bdspeech_recognition_error);
-                //intent.putExtra(EXTRA_SOUND_CANCEL, R.raw.bdspeech_recognition_cancel);
-
-                //  intent.putExtra("grammer", "asset:///baidu_speech_grammar.bsg"); // 设置离线的授权文件(离线模块需要授权), 该语法可以用自定义语义工具生成, 链接http://yuyin.baidu.com/asr#m5
-                //intent.putExtra("slot-data", your slots); // 设置grammar中需要覆盖的词条,如联系人名
-                startActivityForResult(intent, 1);
+                Toast.makeText(getApplicationContext(),"开始语音识别",Toast.LENGTH_SHORT).show();
+                start();
             }
         });
 
+        stop_test_image_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"停止语音识别",Toast.LENGTH_SHORT).show();
+                stop();
+            }
+        });
 
         DisplayMetrics dm = new DisplayMetrics();
 
@@ -126,6 +157,148 @@ public class TestActivity extends Activity implements OnClickListener {
         //add.setWidth(screenWidth/3);
         last.setMaxWidth(screenWidth / 3);
         next.setMaxWidth(screenWidth / 3);
+    }
+    private void start() {
+        System.out.println("MainActivity---start()--------开始");
+        user_ans_text_view.setText("");
+        Map<String, Object> params = new LinkedHashMap<String, Object>();
+        String event = null;
+        event = SpeechConstant.ASR_START; // 替换成测试的event
+
+        params.put(SpeechConstant.ACCEPT_AUDIO_VOLUME, false);
+        params.put(SpeechConstant.VAD,SpeechConstant.VAD_DNN);
+        String json = null; //可以替换成自己的json
+        json = new JSONObject(params).toString(); // 这里可以替换成你需要测试的json
+        asr.send(event, json, null, 0, 0);
+        System.out.println("MainActivity---start-----结束---------json-----------"+json);
+    }
+    private void stop() {
+        System.out.println("MainActivity---stop（）---------------------开始");
+        asr.send(SpeechConstant.ASR_STOP, null, null, 0, 0); //
+    }
+
+
+    private void printLog(String text) {
+        if (logTime) {
+            //text += "  ;time=" + System.currentTimeMillis();
+        }
+
+        int strlen=text.length();
+        System.out.println("printLog()-----------------text="+text+"------length="+strlen);
+        //text += "\n";
+        if(strlen==2){
+            Log.i(getClass().getName(), text);
+            user_ans_text_view.append(text);
+            setUser_ans(text);
+            judgePoint();
+        }
+    }
+
+    private void judgePoint() {
+        String tempAns=getUser_ans();
+
+        switch(currentnum){
+            case 0:
+                if(tempAns.equals("菠萝")){
+                    score.setText("100");
+                    score.setTextColor(Color.parseColor("#EE2C2C"));
+                }
+                else{
+                    score.setText("0");
+                    score.setTextColor(Color.parseColor("#ffcf0a"));
+                }
+                break;
+            case 1:
+                if(tempAns.equals("草莓")){
+                    score.setText("100");
+                    score.setTextColor(Color.parseColor("#EE2C2C"));
+                }
+                else
+                {
+                    score.setText("0");
+                    score.setTextColor(Color.parseColor("#ffcf0a"));
+                }
+                break;
+            case 2:
+                if(tempAns.equals("橘子")){
+                    score.setText("100");
+                    score.setTextColor(Color.parseColor("#EE2C2C"));
+                }
+                else
+                {
+                    score.setText("0");
+                    score.setTextColor(Color.parseColor("#ffcf0a"));
+                }
+                break;
+            case 3:
+                if(tempAns.equals("梨子")){
+                    score.setText("100");
+                    score.setTextColor(Color.parseColor("#EE2C2C"));
+                }
+                else
+                {
+                    score.setText("0");
+                    score.setTextColor(Color.parseColor("#ffcf0a"));
+                }
+                break;
+            case 4:
+                if(tempAns.equals("芒果")){
+                    score.setText("100");
+                    score.setTextColor(Color.parseColor("#EE2C2C"));
+                }
+                else
+                {
+                    score.setText("0");
+                    score.setTextColor(Color.parseColor("#ffcf0a"));
+                }
+                break;
+            case 5:
+                if(tempAns.equals("柠檬")){
+                    score.setText("100");
+                    score.setTextColor(Color.parseColor("#EE2C2C"));
+                }
+                else
+                {
+                    score.setText("0");
+                    score.setTextColor(Color.parseColor("#ffcf0a"));
+                }
+                break;
+            case 6:
+                if(tempAns.equals("苹果")){
+                    score.setText("100");
+                    score.setTextColor(Color.parseColor("#EE2C2C"));
+                }
+                else
+                {
+                    score.setText("0");
+                    score.setTextColor(Color.parseColor("#ffcf0a"));
+                }
+                break;
+        }
+    }
+
+    private void initPermission() {
+        String permissions[] = {Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        ArrayList<String> toApplyList = new ArrayList<String>();
+
+        for (String perm :permissions){
+            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, perm)) {
+                toApplyList.add(perm);
+                //进入到这里代表没有权限.
+
+            }
+        }
+        String tmpList[] = new String[toApplyList.size()];
+        if (!toApplyList.isEmpty()){
+            ActivityCompat.requestPermissions(this, toApplyList.toArray(tmpList), 123);
+        }
+
     }
 
     public void onClick(View v) {
@@ -146,10 +319,13 @@ public class TestActivity extends Activity implements OnClickListener {
                 currentnum = 6;
             this.UpdateView();
             setItem(currentnum);
+
         }
 
         Log.i("3", "3");
     }
+
+
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         // TODO Auto-generated method stub
@@ -176,6 +352,9 @@ public class TestActivity extends Activity implements OnClickListener {
     }
 
     private void UpdateView() {
+        score.setText("暂无");
+        score.setTextColor(Color.parseColor("#ffcf0a"));
+        user_ans_text_view.setText("");
         if (currentnum == 0) {
             last.setEnabled(false);
         } else if (currentnum > 0) {
@@ -239,36 +418,43 @@ public class TestActivity extends Activity implements OnClickListener {
                 picture.setImageResource(R.drawable.boluo);
                 pinyin.setText(pinyinarray[num]);
                 hanzi.setText(sb);
+                std_ans_text_view.setText(sb);
                 break;
             case 1:
                 picture.setImageResource(R.drawable.caomei);
                 pinyin.setText(pinyinarray[num]);
                 hanzi.setText(sb);
+                std_ans_text_view.setText(sb);
                 break;
             case 2:
                 picture.setImageResource(R.drawable.juzi);
                 pinyin.setText(pinyinarray[num]);
                 hanzi.setText(sb);
+                std_ans_text_view.setText(sb);
                 break;
             case 3:
                 picture.setImageResource(R.drawable.lizi);
                 pinyin.setText(pinyinarray[num]);
                 hanzi.setText(sb);
+                std_ans_text_view.setText(sb);
                 break;
             case 4:
                 picture.setImageResource(R.drawable.mangguo);
                 pinyin.setText(pinyinarray[num]);
                 hanzi.setText(sb);
+                std_ans_text_view.setText(sb);
                 break;
             case 5:
                 picture.setImageResource(R.drawable.ningmeng);
                 pinyin.setText(pinyinarray[num]);
                 hanzi.setText(sb);
+                std_ans_text_view.setText(sb);
                 break;
             case 6:
                 picture.setImageResource(R.drawable.pingguo);
                 pinyin.setText(pinyinarray[num]);
                 hanzi.setText(sb);
+                std_ans_text_view.setText(sb);
                 break;
         }
     }
@@ -300,4 +486,37 @@ public class TestActivity extends Activity implements OnClickListener {
     }
 
 
+    @Override
+    public void onEvent(String name, String params, byte[] data, int offset, int length) {
+
+        System.out.println("MainActivity--------------onEvent()");
+        String best_result="";
+        String stdStr="asr.partial";
+        if(name.equals(stdStr)){
+            System.out.println("MainActivity-----onEvent----name.queals(stdStr)="+name.equals(stdStr));
+            //String logTxt = "name: " + name;
+            String logTxt = "";
+
+            if (params != null && !params.isEmpty()) {
+                System.out.println("MainActivity-----onEvent----params"+params);
+                String[] temp=params.split("\"best_result\":\"");
+                System.out.println("MainActivity-----onEvent----params=temp[1]="+params);
+                String[] temp2=temp[1].split("\",\"results_recognition");
+                params=temp2[0];
+                System.out.println("MainActivity-----onEvent----params=temp2[0]="+params);
+                logTxt +=params;
+            }
+            if (name.equals(SpeechConstant.CALLBACK_EVENT_ASR_PARTIAL)) {
+                if (params.contains("\"nlu_result\"")) {
+                    if (length > 0 && data.length > 0) {
+                        logTxt += ", 语义解析结果：" + new String(data, offset, length);
+                    }
+                }
+            } else if (data != null) {
+                logTxt += " ;data length=" + data.length;
+            }
+            printLog(logTxt);
+            System.out.println("MainActivity--------------onEvent()----logTxt"+logTxt);
+        }
+    }
 }
